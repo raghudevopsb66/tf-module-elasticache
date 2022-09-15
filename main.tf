b86cc76fdb9c8386b3c00c2f32c7e1e473de414b
Raghu K <raghuk.vit@gmail.com>
2022-09-15 07:57:36 +0530

    Update tf-module-elasticache/

---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_elasticache_cluster" "main" {
+  cluster_id           = "${var.env}-redis"
+  engine               = "redis"
+  engine_version       = var.engine_version
+  node_type            = var.instance_class
+  num_cache_nodes      = var.instance_count
+  parameter_group_name = aws_elasticache_parameter_group.main.name
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  port                 = 6379
+}
+
+resource "aws_elasticache_subnet_group" "main" {
+  name       = "${var.env}-redis"
+  subnet_ids = var.db_subnets_ids
+
+  tags = {
+    Name = "${var.env}-redis"
+  }
+}
+
+resource "aws_elasticache_parameter_group" "main" {
+  name   = "${var.env}-redis"
+  family = "redis6.x"
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.env}-redis"
+  description = "${var.env}-redis"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "REDIS"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+  tags = {
+    Name = "${var.env}-redis"
+  }
+}
+
