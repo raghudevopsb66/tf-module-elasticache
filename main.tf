79f3ef4a1850b37e22f57cd29ca4253ff2445f3c
Raghu K <raghuk.vit@gmail.com>
2022-10-15 09:08:21 +0530

    Update terraform-modules/tf-module-elasticache/

--- main.tf
+++ main.tf
@@ -53,3 +53,9 @@ resource "aws_ssm_parameter" "elasticache" {
   type  = "String"
   value = aws_elasticache_cluster.main.cache_nodes[0].address
 }
+
+resource "aws_ssm_parameter" "redis-edpoint" {
+  name  = "immutable.redis.endpoint"
+  type  = "String"
+  value = aws_elasticache_cluster.main.cache_nodes[0].address
+}
